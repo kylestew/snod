8d9f5e11a60a30d8e84a35d6c2166c10b4402ad7
Kyle Stewart <kylestew@gmail.com>
2022-03-21 07:39:02 -0700

    add simple math utils

--- math.js
+++ math.js
@@ -4,4 +4,9 @@ function linspace(start, stop, num, endpoint = false) {
   return Array.from({ length: num }, (_, i) => start + step * i);
 }
 
-export { linspace };
+const lerp = (x, y, a) => x * (1 - a) + y * a;
+const clamp = (a, min = 0, max = 1) => Math.min(max, Math.max(min, a));
+const invlerp = (x, y, a) => clamp((a - x) / (y - x));
+const range = (x1, y1, x2, y2, a) => lerp(x2, y2, invlerp(x1, y1, a));
+
+export { linspace, lerp, clamp };
