32f3460605652d046855786d749cdeabc2617f7e
Kyle Stewart <kylestew@gmail.com>
2022-04-18 13:26:25 -0600

    helpful func for color

--- color.js
+++ color.js
@@ -1,5 +1,6 @@
 /* 
-takes a vector 3 of RGB color 
+takes a vector 3 of RGB colors
+input is in [0-255] range
 result is in [0-1] range
 https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
 */
@@ -9,4 +10,15 @@ function luminosity(rgb) {
   return (0.299 * rgb[0] + 0.587 * rgb[1] + 0.144 * rgb[2]) / 255.0;
 }
 
-export { luminosity };
+function hexToRGB(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if (result) {
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+    return [r, g, b];
+  }
+  return null;
+}
+
+export { luminosity, hexToRGB };
